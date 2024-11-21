0996fef3cd05430f5d232c3dd3a0ca4a52f46b6a
RtoRakesh <rakeshrodda1212@gmail.com>
2024-11-21 20:42:12 +0530

    added_switch_case

---
+++ TrafficLight/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TrafficLight from "./TrafficLight";
+
+function App() {
+  return (
+    <>
+      <TrafficLight />
+    </>
+  );
+}
+
+export default App;
---
+++ TrafficLight/src/TrafficLight.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+const TrafficLight = () => {
+  const [currentColor, setCurrentColor] = useState("green");
+
+  useEffect(() => {
+    let timer;
+    switch (currentColor) {
+      case "green":
+        timer = setTimeout(() => setCurrentColor("yellow"), 3000);
+        break;
+      case "yellow":
+        timer = setTimeout(() => setCurrentColor("red"), 2000);
+        break;
+      case "red":
+        timer = setTimeout(() => setCurrentColor("yellow"), 5000);
+        break;
+      default:
+        timer = setTimeout(() => setCurrentColor("green"), 2000);
+    }
+
+    return () => clearTimeout(timer);
+  }, [currentColor]);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          borderRadius: "50%",
+          margin: "0 auto",
+          backgroundColor: currentColor,
+          transition: "background-color 0.5s ease",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default TrafficLight;
